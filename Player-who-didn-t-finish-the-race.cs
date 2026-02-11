18d6631d70b90de8fc9e790eeb36d3f32cd72fd9
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-12 03:31:51 +0900

    Create Player-who-didn-t-finish-the-race.cs

---
+++ Player-who-didn-t-finish-the-race.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public string solution(string[] participant, string[] completion) {
+        Dictionary<string, int> counts = new Dictionary<string, int>(completion.Length);
+
+        foreach (string name in completion) {
+            if (counts.ContainsKey(name))
+                counts[name]++;
+            else
+                counts[name] = 1;
+        }
+
+        foreach (string name in participant) {
+            if (!counts.ContainsKey(name) || counts[name] == 0) {
+                return name;
+            }
+            counts[name]--;
+        }
+
+        return "";
+    }
+}
+
+
+public class Solution {
+    public string solution(string[] participant, string[] completion) {
+        Dictionary<string, int> counts = new Dictionary<string, int>(participant.Length);
+
+        foreach (string name in participant) {
+            if (counts.ContainsKey(name)) counts[name]++;
+            else counts[name] = 1;
+        }
+
+        foreach (string name in completion) {
+            counts[name]--;
+        }
+
+        foreach (var pair in counts) {
+            if (pair.Value > 0) {
+                return pair.Key;
+            }
+        }
+
+        return "";
+    }
+}
