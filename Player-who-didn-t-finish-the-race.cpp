eade314eab62452dfccd53e69da43f3cd7b19e69
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-12 03:27:43 +0900

    Refactor solution function for race completion

--- Player-who-didn-t-finish-the-race.cpp
+++ Player-who-didn-t-finish-the-race.cpp
@@ -7,6 +7,24 @@ using namespace std;
 string solution(vector<string> participant, vector<string> completion)
 {
     unordered_map<string, int> counts;
+    counts.reserve(completion.size());
+
+    for (const auto& name : completion) ++counts[name];
+
+    for (const auto& name : participant)
+    {
+        auto it = counts.find(name);
+        if (it == counts.end() || it->second == 0) return name;
+        --it->second;
+    }
+
+    return "";
+}
+
+
+string solution(vector<string> participant, vector<string> completion)
+{
+    unordered_map<string, int> counts;
     counts.reserve(participant.size());
 
     for (const auto& name : participant) ++counts[name];
@@ -17,3 +35,4 @@ string solution(vector<string> participant, vector<string> completion)
 
     return "";
 }
+
